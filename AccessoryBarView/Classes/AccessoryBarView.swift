60deb4be91038b9aacfa8681c3aea19406175d2b
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2018-05-24 17:54:08 -0300

    init commit

--- AccessoryBarView/Classes/AccessoryBarView.swift
+++ AccessoryBarView/Classes/AccessoryBarView.swift
@@ -1,8 +1,95 @@
-//
-//  AccessoryBarView.swift
-//  InputProgress
-//
-//  Created by Rafael Damasceno on 24/05/18.
-//
-
 import Foundation
+import InputProgress
+
+
+public protocol AccessoryDelegate: class {
+    func accessoryDidContinue()
+}
+
+open class AccessoryBarView: UIView {
+    
+    open var buttomContinue = UIButton()
+    open var inputProgress: InputProgress?
+    open var hideAccessoryView: Bool = false
+    open var accessoryView: UIToolbar?
+    open var accessoryDelegate: AccessoryDelegate?
+    
+    public init() {
+        super.init(frame: UIScreen.main.bounds)
+        setupAccessoryView()
+        setupContinueButton()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setAccessoryViewBackGroundColor(_ color: UIColor) {
+        self.accessoryView?.barTintColor = color
+    }
+    
+    public func setProgressBarColor(_ color: UIColor) {
+        inputProgress?.progressBarColor = color
+    }
+    
+    public func setButtomTitle(_ title: String) {
+        buttomContinue.setTitle(title, for: .normal)
+    }
+    
+    public func setButtonTitleFont(_ font: UIFont) {
+        buttomContinue.titleLabel?.font = font
+    }
+    
+    public func setButtonTitleColor(_ color: UIColor) {
+        buttomContinue.titleLabel?.textColor = color
+    }
+    
+    private func setupAccessoryView() {
+        accessoryView = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 56))
+        accessoryView?.isTranslucent = false
+        accessoryView?.isOpaque = true
+        accessoryView?.barTintColor = UIColor.red
+        let continueBarButton = UIBarButtonItem(customView: buttomContinue)
+        accessoryView?.setItems([continueBarButton], animated: true)
+    }
+    
+    private func setupContinueButton() {
+        buttomContinue.addTarget(self, action: #selector(self.buttomContinueTouched(_:)), for: .touchUpInside)
+        buttomContinue.setTitle("Continuar", for: .normal)
+        buttomContinue.contentHorizontalAlignment = .right
+        buttomContinue.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        buttomContinue.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: 56)
+        buttomContinue.backgroundColor = .clear
+    }
+    
+    public func setupProgressBar(presentedView: UIView, textFields: [UITextField], progress: CGFloat, shouldHideKeyboard: Bool = false, shouldHideAccessoryView: Bool = false, textFieldDelegate: UITextFieldDelegate) {
+        textFields.forEach{
+            $0.delegate = textFieldDelegate
+            $0.inputAccessoryView = accessoryView
+        }
+        inputProgress = InputProgress(presentingView: presentedView, textFields: textFields)
+        inputProgress?.progressBarColor = UIColor.blue
+        inputProgress?.progressBarHeight = 8.0
+        inputProgress?.progress = progress
+        inputProgress?.shouldDisplayBottomViewBar = true
+        self.hideAccessoryView = shouldHideAccessoryView
+    }
+    
+    open override var canBecomeFirstResponder: Bool {
+        return !hideAccessoryView
+    }
+    
+    open override var inputAccessoryView: UIView {
+        get {
+            return self.accessoryView!
+        }
+    }
+    
+    // MARK: IBActions
+    
+    @objc func buttomContinueTouched(_ sender: UIButton) {
+        accessoryDelegate?.accessoryDidContinue()
+    }
+}
+
+
