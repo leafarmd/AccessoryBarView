a4797a627198d681bd66e1c8c679554e352ae559
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2018-05-24 17:57:01 -0300

    init commit

--- AccessoryBarViewController.swift
+++ AccessoryBarViewController.swift
@@ -17,20 +17,20 @@ public protocol AccessoryDelegate: class {
 }
 
 open class AccessoryBarViewController: UIViewController {
-    
-    
-    
+
+
+
     private var hideAccessoryView: Bool = false
     open var accessoryView: UIToolbar?
     open var accessoryDelegate: AccessoryDelegate?
     open var inputProgress: InputProgress?
     open var buttomContinue = UIButton()
-    
+
     open override func viewDidLoad() {
         setupAccessoryView()
         setupContinueButton()
     }
-    
+
     private func setupAccessoryView() {
         accessoryView = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 56))
         accessoryView?.isTranslucent = false
@@ -39,7 +39,7 @@ open class AccessoryBarViewController: UIViewController {
         let continueBarButton = UIBarButtonItem(customView: buttomContinue)
         accessoryView?.setItems([continueBarButton], animated: true)
     }
-    
+
     private func setupContinueButton() {
         buttomContinue.addTarget(self, action: #selector(self.buttomContinueTouched(_:)), for: .touchUpInside)
         buttomContinue.setTitle("Continue", for: .normal)
@@ -48,7 +48,7 @@ open class AccessoryBarViewController: UIViewController {
         buttomContinue.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 56)
         buttomContinue.backgroundColor = .clear
     }
-    
+
     open func setupProgressBar(presentedView: UIView, textFields: [UITextField], progress: CGFloat, shouldHideKeyboard: Bool = false, shouldHideAccessoryView: Bool = false, textFieldDelegate: UITextFieldDelegate) {
         textFields.forEach{
             $0.delegate = textFieldDelegate
@@ -64,46 +64,45 @@ open class AccessoryBarViewController: UIViewController {
             hideKeyboardWhenTappedAround()
         }
     }
-    
+
     open func setButtomTitle(_ title: String) {
         buttomContinue.setTitle(title, for: .normal)
     }
-    
+
     open override var canBecomeFirstResponder: Bool {
         return !hideAccessoryView
     }
-    
+
     open override var inputAccessoryView: UIView {
         get {
             return self.accessoryView!
         }
     }
-    
+
     // MARK: IBActions
-    
+
     @objc func buttomContinueTouched(_ sender: UIButton) {
         accessoryDelegate?.accessoryDidContinue()
     }
 }
 
+
 public extension AccessoryBarViewController {
-    
+
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer =  UITapGestureRecognizer(target: self, action:  #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-    
+
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
 }
 
 extension AccessoryBarViewController: UITextFieldDelegate {
-    
+
     open func textFieldDidBeginEditing(_ textField: UITextField) {
         accessoryDelegate?.textFieldDidBeginEditing(textField)
     }
 }
-
-
